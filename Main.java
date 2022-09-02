40f2712ae49017acc571d08c14ea32cf23f731b1
willpaxton16 <90337820+willpaxton16@users.noreply.github.com>
2022-09-02 11:05:09 -0600

    random generator class

--- Main.java
+++ Main.java
@@ -5,3 +5,13 @@ public class Main
         System.out.println("Hello world");
     }
 }
+
+import java.util.Random;
+class GenerateRandom {
+    public static void main( String args[] ) {
+        Random rand = new Random();
+        int upperbound = 25
+
+        int int_random = rand.nextInt(upperbound)
+    }
+}
